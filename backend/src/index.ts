a72741bc2ef24b3767bb193d088da915b44bb268
OmkumarMangukiya <ommangukiya8+github@gmail.com>
2024-10-12 12:37:57 +0530

    updating

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,9 +1,7 @@
 import { Hono } from 'hono'
-
+import {signin} from './routes/sign'
 const app = new Hono()
 
-app.get('/', (c) => {
-  return c.text('Hello Hono!')
-})
+app.use('/users/signin',signin);
 
 export default app
