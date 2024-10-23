003057536b29d0e9b22d95537faf11a4a93eeb96
OmkumarMangukiya <ommangukiya8+github@gmail.com>
2024-10-23 07:16:44 +0530

    improved ui

--- frontend/src/pages/Warehouse.tsx
+++ frontend/src/pages/Warehouse.tsx
@@ -75,13 +75,23 @@ export default function Warehouse() {
       <div className="max-w-7xl mx-auto">
         <div className="flex justify-between items-center mb-8">
           <h1 className="text-3xl font-bold text-gray-900">Warehouse Products</h1>
+          <div className="flex gap-x-4">
+          <button
+            onClick={() => navigate("/warehouseSales")}
+            className="bg-white text-blue-500 px-6 py-2 rounded-lg hover:text-blue-600 shadow-lg hover:shadow-xl hover:shadow-gray-400 font-medium transition duration-300"
+          >
+            Sell
+          </button>
           <button
             onClick={() => navigate("/addproduct")}
-            className="bg-blue-600 text-white px-4 py-2 rounded-lg flex items-center hover:bg-blue-700 transition duration-300"
+            className="bg-blue-600 text-white px-4 py-2 rounded-lg hover:shadow-lg hover:shadow-gray-400 flex items-center hover:bg-blue-700 transition duration-300"
           >
             <PlusIcon className="w-5 h-5 mr-2" />
             Add Product
           </button>
+          {/* <div className="mt-6 text-right"> */}
+          
+        </div>
         </div>
 
         <div className="bg-white rounded-lg shadow-md overflow-hidden">
@@ -138,14 +148,7 @@ export default function Warehouse() {
           </table>
         </div>
 
-        <div className="mt-6 text-right">
-          <button
-            onClick={() => navigate("/warehouseSales")}
-            className="bg-green-600 text-white px-6 py-2 rounded-lg hover:bg-green-700 transition duration-300"
-          >
-            Sell
-          </button>
-        </div>
+        
       </div>
     </div>
   )
